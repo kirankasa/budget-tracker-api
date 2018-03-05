6829f7151b738ed479c1173cd37a2202a2e7fab2
kiranreddykasa <kiranreddy2004@gmail.com>
2018-03-05 23:35:39 +0530

    partial transaction

---
+++ src/main/java/com/kiranreddy/budgettracker/transaction/Transaction.java
@@ -0,0 +1,75 @@
+package com.kiranreddy.budgettracker.transaction;
+
+import java.util.Date;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+
+@Entity
+public class Transaction {
+
+	@Id
+	@GeneratedValue
+	private Long id;
+
+	private String type;
+
+	private Double amount;
+
+	private Date date;
+
+	private String note;
+	
+	public Transaction() {
+	}
+
+	public Transaction(Long id, String type, Double amount, Date date, String note) {
+		this.id = id;
+		this.type = type;
+		this.amount = amount;
+		this.date = date;
+		this.note = note;
+	}
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public String getType() {
+		return type;
+	}
+
+	public void setType(String type) {
+		this.type = type;
+	}
+
+	public Double getAmount() {
+		return amount;
+	}
+
+	public void setAmount(Double amount) {
+		this.amount = amount;
+	}
+
+	public Date getDate() {
+		return date;
+	}
+
+	public void setDate(Date date) {
+		this.date = date;
+	}
+
+	public String getNote() {
+		return note;
+	}
+
+	public void setNote(String note) {
+		this.note = note;
+	}
+
+}
---
+++ src/test/java/com/kiranreddy/budgettracker/transaction/TransactionRepositoryTest.java
@@ -0,0 +1,47 @@
+package com.kiranreddy.budgettracker.transaction;
+
+import java.util.Date;
+import java.util.List;
+
+import org.assertj.core.api.Assertions;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@DataJpaTest
+public class TransactionRepositoryTest {
+
+	@Autowired
+	private TransationRepository transationRepository;
+	
+	@Test
+	public void saveUserTest() {
+		Date date =new Date();
+		Transaction transaction = new Transaction(1L,"type",100.00,date, "note");
+		transationRepository.save(transaction);
+		
+		Assertions.assertThat(transaction.getId()).isNotNull().isPositive();
+		Assertions.assertThat(transaction.getAmount()).isEqualTo(100.00);
+		Assertions.assertThat(transaction.getType()).isEqualTo("type");
+		Assertions.assertThat(transaction.getDate()).isEqualTo(date);
+		Assertions.assertThat(transaction.getNote()).isEqualTo("note");
+	}
+	
+	@Test
+	public void fetchUsersTest() {
+		Date date =new Date();
+		Transaction transaction = new Transaction(1L,"type",100.00,date, "note");
+		transationRepository.save(transaction);
+		List<Transaction> transactions = transationRepository.findAll();
+		Assertions.assertThat(transactions).hasSize(1);
+		
+		Assertions.assertThat(transactions.iterator().next().getId()).isNotNull().isPositive();
+		Assertions.assertThat(transactions.iterator().next().getAmount()).isEqualTo(100.00);
+		Assertions.assertThat(transactions.iterator().next().getType()).isEqualTo("type");
+		Assertions.assertThat(transactions.iterator().next().getDate()).isEqualTo(date);
+		Assertions.assertThat(transactions.iterator().next().getNote()).isEqualTo("note");
+	}
+}
