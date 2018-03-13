9dda216a75c1ebdbcd3f93743a032fe8468a6169
kiranreddykasa <kiranreddy2004@gmail.com>
2018-03-13 20:04:14 +0530

    added hiberate dialect

--- src/main/java/com/kiranreddy/budgettracker/user/User.java
+++ src/main/java/com/kiranreddy/budgettracker/user/User.java
@@ -2,6 +2,7 @@ package com.kiranreddy.budgettracker.user;
 
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
 import javax.persistence.Id;
 
 import com.fasterxml.jackson.annotation.JsonProperty;
@@ -11,7 +12,7 @@ import com.fasterxml.jackson.annotation.JsonProperty.Access;
 public class User {
 
 	@Id
-	@GeneratedValue
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Long id;
 
 	private String firstName;
