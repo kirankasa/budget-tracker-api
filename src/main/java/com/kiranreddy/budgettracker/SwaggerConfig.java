74288011b6b1697d8863890dda5668557c12daa5
kiranreddykasa <kiranreddy2004@gmail.com>
2018-03-27 19:29:04 +0530

    swagger config

---
+++ src/main/java/com/kiranreddy/budgettracker/SwaggerConfig.java
@@ -0,0 +1,30 @@
+package com.kiranreddy.budgettracker;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.bind.annotation.RestController;
+
+import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.service.ApiInfo;
+import springfox.documentation.service.Contact;
+import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spring.web.plugins.Docket;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
+
+@Configuration
+@EnableSwagger2
+public class SwaggerConfig {
+	@Bean
+	public Docket productApi() {
+		return new Docket(DocumentationType.SWAGGER_2).select()
+				.apis(RequestHandlerSelectors.withClassAnnotation(RestController.class)).build().apiInfo(metaData());
+	}
+
+	private ApiInfo metaData() {
+		ApiInfo apiInfo = new ApiInfo("Budget Tracker", "REST API for budget tracker", "1.0", "Terms of service",
+				new Contact("Kiran kumar Reddy k", null, "kiranreddy2004@gmail.com"),
+				"Apache License Version 2.0", "https://www.apache.org/licenses/LICENSE-2.0");
+		return apiInfo;
+	}
+
+}
