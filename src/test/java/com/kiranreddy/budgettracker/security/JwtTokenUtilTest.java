6a7bfdc3ddebdba6079ec759925ecf04f46aef5b
kiranreddykasa <kiranreddy2004@gmail.com>
2018-05-03 22:37:37 +0530

    updated jwt test cases

---
+++ src/test/java/com/kiranreddy/budgettracker/security/JwtTokenUtilTest.java
@@ -0,0 +1,33 @@
+package com.kiranreddy.budgettracker.security;
+
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class JwtTokenUtilTest {
+
+    @Before
+    public void setUp() throws Exception {
+    }
+
+    @Test
+    public void getUsernameFromToken() {
+    }
+
+    @Test
+    public void getIssuedAtDateFromToken() {
+    }
+
+    @Test
+    public void getClaimFromToken() {
+    }
+
+    @Test
+    public void generateToken() {
+    }
+
+    @Test
+    public void validateToken() {
+    }
+}
---
+++ src/test/java/com/kiranreddy/budgettracker/security/JwtUserDetailsServiceTest.java
@@ -0,0 +1,12 @@
+package com.kiranreddy.budgettracker.security;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class JwtUserDetailsServiceTest {
+
+    @Test
+    public void loadUserByUsername() {
+    }
+}
