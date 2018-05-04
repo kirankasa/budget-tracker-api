dd66f78a91e73dac13b73a4295cf7e31a814a3f9
kiranreddykasa <kiranreddy2004@gmail.com>
2018-05-04 19:47:03 +0530

    updated unit test cases

---
+++ src/test/java/com/kiranreddy/budgettracker/auth/AuthenticationRestControllerTest.java
@@ -0,0 +1,20 @@
+package com.kiranreddy.budgettracker.auth;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class AuthenticationRestControllerTest {
+
+    @Test
+    public void createAuthenticationToken() {
+    }
+
+    @Test
+    public void handleAuthenticationException() {
+    }
+
+    @Test
+    public void getAuthenticatedUser() {
+    }
+}
